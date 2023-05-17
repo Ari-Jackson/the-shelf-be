24ae22b708d4f4cd329d5aeeb1addfe28b83e16f
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-05-17 10:07:29 -0400

    🗺️ Updates schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,13 @@ CREATE DATABASE books_dev;
 
 CREATE TABLE books (
  id SERIAL PRIMARY KEY,
- title TEXT NOT NULL,
+ title VARCHAR(500) NOT NULL,
  format TEXT,
  genre VARCHAR(255),
- pages INT,
+ pages INT CHECK (pages > 0),
  theme VARCHAR(255) DEFAULT 'General',
- rating INT CHECK (rating >= 0 AND rating <= 10)
+ rating INT CHECK (rating >= 0 AND rating <= 10),
+ isFavorite BOOLEAN
 );
 
 \d books;
--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -14,7 +14,7 @@ bookRouter
   .route("/")
   .get(async (req, res) => {
     const { error, result } = await getAllBooks();
-    if (error) {
+    if (!!error) {
       res.status(500).json({ error });
     } else {
       res.status(200).json(result);
@@ -34,7 +34,7 @@ bookRouter
   .get(async (req, res) => {
     const { id } = req.params;
     const { error, result } = await getOneBook(id);
-    if (error === 0) {
+    if (!!error) {
       res.status(404).json({ error: "bookmark not found" });
     } else {
       res.status(200).json(result);
@@ -43,7 +43,7 @@ bookRouter
   .put(bookValidator, async (req, res) => {
     const { id } = req.params;
     const { error, result } = await updateOneBook(id, req.body);
-    if (error) {
+    if (!!error) {
       res.status(500).json({ error: "server error" });
     } else {
       res.status(200).json(result);
@@ -52,7 +52,7 @@ bookRouter
   .delete(async (req, res) => {
     const { id } = req.params;
     const { error, result } = await deleteOneBook(id);
-    if (error) {
+    if (!!error) {
       res.status(404).json("Bookmark not found");
     } else {
       res.status(201).json(result);
